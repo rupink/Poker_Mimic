b6fe633de291861fd2eb3aca1833f008e06c725b
rupink <66811124+rupink@users.noreply.github.com>
2023-04-15 21:17:21 -0400

    Create Card.cpp

---
+++ Card.cpp
@@ -0,0 +1,24 @@
+#include "Card.h"
+#include <iostream>
+
+void printCard(Card card) {
+    std::string value;
+    switch(card.value) {
+        case 1:
+            value = "Ace";
+            break;
+        case 11:
+            value = "Jack";
+            break;
+        case 12:
+            value = "Queen";
+            break;
+        case 13:
+            value = "King";
+            break;
+        default:
+            value = std::to_string(card.value);
+            break;
+    }
+    std::cout << value << " of " << card.suit << std::endl;
+}
