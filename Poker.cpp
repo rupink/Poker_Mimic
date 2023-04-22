98066ce3ec7c4f2b27d040937471ca9e27f1a1ec
rupink <66811124+rupink@users.noreply.github.com>
2023-04-21 22:30:06 -0400

    Update Poker.cpp

--- Poker.cpp
+++ Poker.cpp
@@ -9,6 +9,28 @@ std::string determineWinner(std::vector<Card>& playerHand, std::vector<Card>& de
     return "Player";
 }
 
+// Deal out the flop (3 community cards)
+void dealFlop(std::vector<Card>& communityCards, Deck& deck)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        communityCards.push_back(deck.drawCard());
+    }
+}
+
+// Deal out the turn (1 community card)
+void dealTurn(std::vector<Card>& communityCards, Deck& deck)
+{
+    communityCards.push_back(deck.drawCard());
+}
+
+// Deal out the river (1 community card)
+void dealRiver(std::vector<Card>& communityCards, Deck& deck)
+{
+    communityCards.push_back(deck.drawCard());
+}
+
+
 int main() {
     std::vector<Card> deck = createDeck();
     shuffleDeck(deck);
